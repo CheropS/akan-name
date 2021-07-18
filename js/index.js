3120d530ec003c4f0cde70b351d248fd7be258da
Sharon Cherop <sh.cherop@gmail.com>
2021-07-18 18:13:10 +0300

    validating form using js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,24 @@
 function validateform() {
-    var firstName=document.myform.name.value;
+    var firstName=document.getElementsByName("firstName");
     var date=document.myform.date.value;
     var month=document.myform.month.value;
     var year=document.myform.year.value;
+    console.log(year)
+
+    var newDate=parseInt(date);
+    var newYear=parseInt(year);
 
     if(firstName==null ||firstName=="") {
         alert("Name can't be blank");
         return false;
-    } else if (date==) {
+    } else if (date>31) {
         alert("date is invalid");
         return false;
-    } else if (month)
+    } else if (month>12) {
+        alert("month is invalid");
+    } else if (year>2021) {
+        alert("year is invalid");
+    }
+    alert(newYear)
 
 }
