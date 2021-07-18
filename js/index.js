36b47177b61ff30fe3b310cced97d1b1474eba5f
Sharon Cherop <sh.cherop@gmail.com>
2021-07-18 15:08:30 +0300

    validating form

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function validateform() {
+    var firstName=document.myform.name.value;
+    var date=document.myform.date.value;
+    var month=document.myform.month.value;
+    var year=document.myform.year.value;
+
+    if(firstName==null ||firstName=="") {
+        alert("Name can't be blank");
+        return false;
+    } else if (date==) {
+        alert("date is invalid");
+        return false;
+    } else if (month)
+
+}
