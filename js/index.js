e1070a1f1fa6822f20b2787a6dc702b8fddda785
Sharon Cherop <sh.cherop@gmail.com>
2021-07-18 20:32:59 +0300

    getting gender function

--- js/index.js
+++ js/index.js
@@ -26,3 +26,14 @@ function validateform() {
     alert(newYear)
 
 }
+
+function getGender() {
+    var gender=document.getElementsByName("dropdown");
+    if(gender[0].onselect==true){
+        var gender="male";
+    } else if (gender[1].onselect==true) {
+        var gender="female";
+    } else {
+        return true;
+    }
+}
