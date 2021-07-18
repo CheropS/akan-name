f361bae1e5742a82bda1a3e4f53c74eee809981c
Sharon Cherop <sh.cherop@gmail.com>
2021-07-18 20:23:12 +0300

    adding arrays on week and names

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+var days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
 function validateform() {
     var firstName=document.getElementsByName("firstName");
     var date=document.myform.date.value;
