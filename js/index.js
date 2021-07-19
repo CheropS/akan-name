ef9e600103a1e4d58589a294a1ad2afdb9dd41ac
Sharon Cherop <sh.cherop@gmail.com>
2021-07-19 06:24:37 +0300

    fixed alert issues on js

--- js/index.js
+++ js/index.js
@@ -1,37 +1,38 @@
-var days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+// var days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+// var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 function validateform() {
-    var firstName=document.getElementsByName("firstName");
-    var date=document.myform.date.value;
-    var month=document.myform.month.value;
-    var year=document.myform.year.value;
-    console.log(year)
+    var gender=document.getElementsByName("dropdown").value;
+    var dd=document.getElementsByName("date").value;
+    var mm=document.getElementsByName("month").value;
+    var cc=document.getElementsByName("centuary").value;
+    var yy=document.getElementsByName("year").value;
 
-    var newDate=parseInt(date);
-    var newYear=parseInt(year);
+    var newDate=parseInt(dd);
+    var newYear=parseInt(yy);
 
-    if(firstName==null ||firstName=="") {
-        alert("Name can't be blank");
+    if(gender=="") {
+        alert("Gender can't be blank");
         return false;
-    } else if (date>31) {
+    } else if (dd>31) {
         alert("date is invalid");
         return false;
-    } else if (month>12) {
+    } else if (mm>12) {
         alert("month is invalid");
-    } else if (year>2021) {
-        alert("year is invalid");
+    } else if (cc>21) {
+        alert("centuary is invalid");
+    } else if (yy>21) {
+        alert("Year is invalid");
     }
     alert(newYear)
-
 }
 
-function getGender() {
-    var gender=document.getElementsByName("dropdown");
-    if(gender[0].onselect==true){
-        var gender="male";
-    } else (gender[1].onselect==true) {
-        var gender="female";
-    }
-}
+
+// function getGender() {
+//     var gender=document.getElementsByName("dropdown");
+//     if(gender[0].onselect==true){
+//         var gender="male";
+//     } else (gender[1].onselect==true) {
+//         var gender="female"; }
+        
